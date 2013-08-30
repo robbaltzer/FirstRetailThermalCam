f13f30256c038f8bbf430193f5bd34086475be33
Rob Baltzer <rob.baltzer01@gmail.com>
2013-08-29 21:33:17 -0700

    Lepton driver changes

--- drivers/lepton/lepton.c
+++ drivers/lepton/lepton.c
@@ -188,14 +188,11 @@ static int __devinit lepton_probe(struct spi_device *spi)
 	if (down_interruptible(&lepton_dev.spi_sem))
 		return -EBUSY;
 
-	printk(KERN_ALERT "lepton_probe1\n");
 	lepton_dev.spi_device = spi;
-	printk(KERN_ALERT "lepton_probe2\n");
+
 	up(&lepton_dev.spi_sem);
-	printk(KERN_ALERT "lepton_probe3\n");
 	/* Try communicating with the device. */
 	ret = lepton_transfer(spi);
-	printk(KERN_ALERT "lepton_probe4\n");
 	return ret;
 }
 
@@ -251,73 +248,74 @@ static int lepton_remove(struct spi_device *spi_device)
 	return 0;
 }
 
-static int __init add_lepton_device_to_bus(void)
-{
-	struct spi_master *spi_master;
-	struct spi_device *spi_device;
-	struct device *pdev;
-	char buff[64];
-	int status = 0;
+// static int __init add_lepton_device_to_bus(void)
+// {
+// 	struct spi_master *spi_master;
+// 	struct spi_device *spi_device;
+// 	struct device *pdev;
+// 	char buff[64];
+// 	int status = 0;
 
-	spi_master = spi_busnum_to_master(SPI_BUS);
-	if (!spi_master) {
-		printk(KERN_ALERT "spi_busnum_to_master(%d) returned NULL\n",
-			SPI_BUS);
-		printk(KERN_ALERT "Missing modprobe omap2_mcspi?\n");
-		return -1;
-	}
+// 	spi_master = spi_busnum_to_master(SPI_BUS);
+// 	if (!spi_master) {
+// 		printk(KERN_ALERT "spi_busnum_to_master(%d) returned NULL\n",
+// 			SPI_BUS);
+// 		printk(KERN_ALERT "Missing modprobe omap2_mcspi?\n");
+// 		return -1;
+// 	}
 
-	spi_device = spi_alloc_device(spi_master);
-	if (!spi_device) {
-		put_device(&spi_master->dev);
-		printk(KERN_ALERT "spi_alloc_device() failed\n");
-		return -1;
-	}
+// 	spi_device = spi_alloc_device(spi_master);
+// 	if (!spi_device) {
+// 		put_device(&spi_master->dev);
+// 		printk(KERN_ALERT "spi_alloc_device() failed\n");
+// 		return -1;
+// 	}
 
-	spi_device->chip_select = SPI_BUS_CS1;
+// 	spi_device->chip_select = SPI_BUS_CS1;
 
-/* Check whether this SPI bus.cs is already claimed */
-	snprintf(buff, sizeof(buff), "%s.%u",
-		dev_name(&spi_device->master->dev),
-		spi_device->chip_select);
+// /* Check whether this SPI bus.cs is already claimed */
+// 	snprintf(buff, sizeof(buff), "%s.%u",
+// 		dev_name(&spi_device->master->dev),
+// 		spi_device->chip_select);
 
-	pdev = bus_find_device_by_name(spi_device->dev.bus, NULL, buff);
-	if (pdev) {
-/* We are not going to use this spi_device, so free it */
-		spi_dev_put(spi_device);
+// 	pdev = bus_find_device_by_name(spi_device->dev.bus, NULL, buff);
+// 	if (pdev) {
+//  We are not going to use this spi_device, so free it 
+// 		spi_dev_put(spi_device);
 
-/*
-* There is already a device configured for this bus.cs
-* It is okay if it us, otherwise complain and fail.
-*/
-	if (pdev->driver && pdev->driver->name &&
-		strcmp(this_driver_name, pdev->driver->name)) {
-		printk(KERN_ALERT
-			"Driver [%s] already registered for %s\n",
-			pdev->driver->name, buff);
-		status = -1;
-	}
-	} else {
-	spi_device->max_speed_hz = SPI_BUS_SPEED;
-	spi_device->mode = SPI_MODE_0;
-	spi_device->bits_per_word = 8;
-	spi_device->irq = -1;
-	spi_device->controller_state = NULL;
-	spi_device->controller_data = NULL;
-	strlcpy(spi_device->modalias, this_driver_name, SPI_NAME_SIZE);
+// /*
+// * There is already a device configured for this bus.cs
+// * It is okay if it us, otherwise complain and fail.
+// */
+// 	if (pdev->driver && pdev->driver->name &&
+// 		strcmp(this_driver_name, pdev->driver->name)) {
+// 		printk(KERN_ALERT
+// 			"Driver [%s] already registered for %s\n",
+// 			pdev->driver->name, buff);
+// 		status = -1;
+// 	}
+// 	} else {
+// 	printk(KERN_ALERT "Set bus speed");
+// 	spi_device->max_speed_hz = SPI_BUS_SPEED;
+// 	spi_device->mode = SPI_MODE_0;
+// 	spi_device->bits_per_word = 8;
+// 	spi_device->irq = -1;
+// 	spi_device->controller_state = NULL;
+// 	spi_device->controller_data = NULL;
+// 	strlcpy(spi_device->modalias, this_driver_name, SPI_NAME_SIZE);
 
-	status = spi_add_device(spi_device);	
-	if (status < 0) {	
-		spi_dev_put(spi_device);
-		printk(KERN_ALERT "spi_add_device() failed: %d\n",
-			status);	
-	}	
-}
+// 	status = spi_add_device(spi_device);	
+// 	if (status < 0) {	
+// 		spi_dev_put(spi_device);
+// 		printk(KERN_ALERT "spi_add_device() failed: %d\n",
+// 			status);	
+// 	}	
+// }
 
-put_device(&spi_master->dev);
+// put_device(&spi_master->dev);
 
-return status;
-}
+// return status;
+// }
 
 
 static struct spi_driver lepton_driver = {
@@ -341,12 +339,12 @@ static int __init lepton_init_spi(void)
 		return error;
 	}
 
-	error = add_lepton_device_to_bus();
-	if (error < 0) {
-		printk(KERN_ALERT "add_lepton_to_bus() failed\n");
-		spi_unregister_driver(&lepton_driver);
-		return error;
-	}
+	// error = add_lepton_device_to_bus();
+	// if (error < 0) {
+	// 	printk(KERN_ALERT "add_lepton_to_bus() failed\n");
+	// 	spi_unregister_driver(&lepton_driver);
+	// 	return error;
+	// }
 
 	return 0;
 }
@@ -437,18 +435,6 @@ fail_2:
 
 fail_1:
 	return -1;
-
-
-/*
-	printk(KERN_ALERT "lepton_init\n");
-
-	memset(&lepton_dev, 0, sizeof(lepton_dev));
-
-	sema_init(&lepton_dev.spi_sem, 1);
-	sema_init(&lepton_dev.fop_sem, 1);
-
-	lepton_init_cdev();
-	return spi_register_driver(&lepton_driver); */
 }
 
 module_init(lepton_init);
@@ -470,7 +456,7 @@ static void __exit lepton_exit(void)
 
 	printk(KERN_ALERT "lepton_exit\n");
 	
-	// spi_unregister_driver(&lepton_driver);
+
 }
 
 module_exit(lepton_exit);
